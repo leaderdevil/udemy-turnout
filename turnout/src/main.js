f4d3ebe7952b84217a90dd948f7e55b690fb4dfd
Pannatorn <peary.11793@hotmail.com>
2018-03-27 14:15:59 +0700

    - Setting Up Turnout > Include Bootstrap > Create Vue object > Create custom .css file for App > Create custom HTML template in App.vue

--- turnout/src/main.js
+++ turnout/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './components/App.vue'
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
+new Vue( {
+    el: '#app',
+    render: h => h(App)
 })
